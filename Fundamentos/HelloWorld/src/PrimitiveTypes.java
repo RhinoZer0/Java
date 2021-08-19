2ba1ea24589c0be2c73dd7bc29c01e3975a02f47
Javier Aguilera <javier.aguilera@continental.com>
2021-08-19 00:44:34 -0500

    Testing with "var" data type to infere data types automatically.

--- Fundamentos/HelloWorld/src/PrimitiveTypes.java
+++ Fundamentos/HelloWorld/src/PrimitiveTypes.java
@@ -12,16 +12,31 @@ public class PrimitiveTypes {
     public static void main(String args[]){
         int i32MyVariable = 10;
         System.out.println(i32MyVariable);
+        System.out.println("i32MyVariable = " + i32MyVariable);
         
         //Modify i32MyVariable value
         i32MyVariable = 5;
         System.out.println(i32MyVariable);
+        System.out.println("i32MyVariable = " + i32MyVariable);
         
         String sMyStringVariable = "Greetings";
         System.out.println(sMyStringVariable);
+        System.out.println("sMyStringVariable = " + sMyStringVariable);
         
         //Modify sMyStringVariable value
         sMyStringVariable = "Goodbye";
         System.out.println(sMyStringVariable);
+        System.out.println("sMyStringVariable = " + sMyStringVariable);
+        
+        //var - infere data types
+        var vMyVariable2 = 15;
+        System.out.println(vMyVariable2);
+        System.out.println("vMyVariable2 = " + vMyVariable2);
+        
+        //Use var type to save a string
+        var vMyStringVariable2 = "Use var to infere string literal";
+        System.out.println(vMyStringVariable2);
+        System.out.println("vMyStringVariable2 = " + vMyStringVariable2);
+        
     }    
 }
