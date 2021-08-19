d003f8a3f3fb3a9cd9d08144991a68efa0ee4b05
Javier Aguilera <javier.aguilera@continental.com>
2021-08-19 00:29:18 -0500

    Included PrimitiveTypes and Greetings classes.

---
+++ Fundamentos/HelloWorld/src/Greetings.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author uidj7796
+ */
+public class Greetings {
+    public static void main(String args[]){
+        System.out.println("Hola Mundo desde Java"); 
+    }
+}
---
+++ Fundamentos/HelloWorld/src/PrimitiveTypes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author uidj7796
+ */
+public class PrimitiveTypes {
+    public static void main(String args[]){
+        int i32MyVariable = 10;
+        System.out.println(i32MyVariable);
+        
+        //Modify i32MyVariable value
+        i32MyVariable = 5;
+        System.out.println(i32MyVariable);
+        
+        String sMyStringVariable = "Greetings";
+        System.out.println(sMyStringVariable);
+        
+        //Modify sMyStringVariable value
+        sMyStringVariable = "Goodbye";
+        System.out.println(sMyStringVariable);
+    }    
+}
