c82c993ed79ed3926b6b52cf5fd9cb2ccb8ec7a6
Javier Aguilera <javier.aguilera@continental.com>
2021-09-01 00:11:10 -0500

    Input a string from console and compare it with a hardcoded string using String.equals() method.

---
+++ Fundamentos/HelloWorld/src/StringCompare.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author uidj7796
+ */
+public class StringCompare {
+    public static void main(String args[]){
+        Scanner console = new Scanner(System.in);
+        String inputString = console.nextLine();
+        String originalString = "Hello";
+        
+        boolean result = inputString.equals(originalString);
+        System.out.println("result = " + result);
+    }
+    
+}
