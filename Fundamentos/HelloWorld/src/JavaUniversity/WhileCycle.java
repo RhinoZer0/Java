fc3453787333a3534442f0cf87d4074bd3fa692e
Javier Aguilera <javier.aguilera@continental.com>
2021-09-15 19:10:40 -0500

    While loop example

---
+++ Fundamentos/HelloWorld/src/JavaUniversity/WhileCycle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaUniversity;
+
+/**
+ *
+ * @author uidj7796
+ */
+public class WhileCycle {
+    public static void main(String args[])
+    {
+        int counter = 0;
+        
+        while(counter < 3)
+        {
+            System.out.println("counter = " + counter);
+            counter++;
+        }
+    }    
+}
