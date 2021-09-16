b368248b4cf3449e41827dd346e4184e887f7d06
Javier Aguilera <javier.aguilera@continental.com>
2021-09-15 20:16:29 -0500

    Crated a new Person class in classes package

---
+++ Fundamentos/HelloWorld/src/classes/Person.java
@@ -0,0 +1,16 @@
+package classes;
+
+/**
+ *
+ * @author uidj7796
+ */
+public class Person {
+    String name;
+    String lastName;
+    
+    public void showInformation()
+    {
+        System.out.println("Name: " + name);
+        System.out.println("lastName: " + lastName);
+    }
+}
