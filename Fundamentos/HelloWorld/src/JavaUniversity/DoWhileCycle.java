5ad4e03af052a44b39ac82704d3bc3eca86f82eb
Javier Aguilera <javier.aguilera@continental.com>
2021-09-15 19:16:09 -0500

    Do while loop example

---
+++ Fundamentos/HelloWorld/src/JavaUniversity/DoWhileCycle.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaUniversity;
+
+/**
+ *
+ * @author uidj7796
+ */
+public class DoWhileCycle {
+    public static void main(String args[])
+    {
+        int counter = 0;
+        
+        do{
+            System.out.println("counter = " + counter);
+            counter++;
+        }while(counter < 0 );
+    }    
+}
