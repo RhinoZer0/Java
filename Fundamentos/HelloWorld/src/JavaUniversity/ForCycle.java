92e475b9540d06c9c6d4432079c0e575c5f14a14
Javier Aguilera <javier.aguilera@continental.com>
2021-09-15 20:01:09 -0500

    For loop exercise

---
+++ Fundamentos/HelloWorld/src/JavaUniversity/ForCycle.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaUniversity;
+
+/**
+ *
+ * @author uidj7796
+ */
+public class ForCycle {
+    public static void main(String args[])
+    {
+        for(int counter = 0; counter < 3; counter++)
+        {
+            System.out.println("counter = " + counter);
+        }
+    }
+}
